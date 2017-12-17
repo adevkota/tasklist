b2ba7f426a965b7b9d2873b006e12b36b400996e
Ashutosh Devkota <ashutosh.devkota@gmail.com>
2017-12-16 19:17:21 -0600

    refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,38 +12,47 @@ export class AppComponent implements OnInit {
   public taskContainer;
 
   constructor(private messageService: MessageService) { 
-   
+    this.messageService.getMessage().subscribe((message) => {
+      this.taskContainer.map((item) => {
+            
+        if(item.index == message.data.listIndex) {
+          //if currindex = tasklist index, remove info from the tasks
+            item.tasks.splice(message.data.index, 1)
+        } else if(item.index == message.data.listIndex -1 && message.text ==="prev") {
+          //if tasklist index = currindex + 1 and event is next, add it
+          item.tasks.push(message.data.info)
+        }
+        else if(item.index == message.data.listIndex +1 && message.text ==="next") {
+          //if tasklis index = curr index -1 and event is prev, add
+          item.tasks.push(message.data.info)
+        }
+      })
+
+    }) 
   }
 
   public ngOnInit() {
-    this.taskContainer = {
-      one: {
+    this.taskContainer = [
+      {
         title: "one",
         index:0,
         tasks:["one", "two"]
       },
-      two: {
+     {
         title: "two",
-
         index:1,
         tasks:["one", "two"]
       },
-      three: {
+       {
         index:2,
         title: "three",
         tasks:["one", "two"]
       },
-      four: {
+      {
         index:3,
         title: "four",
         tasks:["one", "two"]
       }
-    }
+    ]
   }
-  // public addTask(newTask)  {
-  //   this.messageService.sendMessage("newTask", newTask);
-  // }
-  // public search(text)  {
-  //   this.messageService.sendMessage("search", text);
-  // }
 }
--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -11,20 +11,7 @@ export class TaskListComponent implements OnInit {
   public searchText;
   @Input() task;
   constructor(private messageService: MessageService) {
-    this.messageService.getMessage().subscribe((message) => {
-      if(this.task.index == message.data.listIndex) {
-        //if currindex = tasklist index, remove info from the tasks
-        this.task.tasks.splice(message.data.index, 1)
-      } else if(this.task.index == message.data.listIndex -1 && message.text ==="prev") {
-        //if tasklist index = currindex + 1 and event is next, add it
-        this.task.tasks.push(message.data.info)
-      }
-      else if(this.task.index == message.data.listIndex +1 && message.text ==="next") {
-        //if tasklis index = curr index -1 and event is prev, add
-        this.task.tasks.push(message.data.info)
-      }
-      console.log(message.data);
-    }) 
+    
     }
 
   ngOnInit() {
@@ -33,6 +20,5 @@ export class TaskListComponent implements OnInit {
   public addCard() {
     let result = window.prompt("please enter info");
     this.task.tasks.push(result);
-    console.log(this.task);
   }
 }
